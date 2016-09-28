de08a563e7880be5b4c8930468c95a36dd9ee8ca
balusha <baloo@ngs.ru>
2016-09-28 22:17:20 +0700

    smthng strange

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "UserPages", type: :request do
+
+  subject { page }
+
+  describe "Sign up page" do
+
+    before {visit signup_path}
+
+    it{should have_content 'Sign up'}
+    it{should have_title 'Sign up'}
+
+  end
+end
