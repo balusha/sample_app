e1434b368982e60c7481e639e0f462d2f2797411
exit <exit>
2016-10-14 10:51:02 +0700

    signin added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,17 @@
 class SessionsController < ApplicationController
 
+  include SessionsHelper  
+
   def new
 
   end
 
   def create
+
     user = User.find_by_email params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
-
+      sign_in user
+      redirect_to current_user
     else
       flash.now[:error] = "Invalid user login/password"
       render 'new'
