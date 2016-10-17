34487d4dc31a30b376c1faaabce3911a60d86e70
balusha <baloo@ngs.ru>
2016-10-17 22:46:22 +0700

    added sign in out menu

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,8 +11,24 @@ module SessionsHelper
 	end
 
 	def current_user
+		if !cookies[:remember_token].nil?
 		token = User.encrypt cookies[:remember_token]
 		@current_user ||= User.find_by(remember_token: token)
+		else
+			nil
+		end
+
+	end
+
+	def signed_in?
+		!current_user.nil?
+	end
+
+	def sign_out
+		cu = current_user
+		cookies.delete(:remember_token)
+		token = User.new_remember_token
+		cu.update_attribute(:remember_token, User.encrypt(token))
 	end
 
 end
